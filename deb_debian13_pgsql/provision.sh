e764680f7cace793783589689ca52bcd071cbd41
Pierre Neyron <pierre.neyron@imag.fr>
2024-12-18 18:32:00 +0100

    [debian13] Get node resource definition from the server VM specs.

--- deb_debian13_pgsql/provision.sh
+++ deb_debian13_pgsql/provision.sh
@@ -210,7 +210,10 @@ EOF
     stamp="create oar resources"
     [ -e /tmp/stamp.${stamp// /_} ] || (
       echo -ne "##\n## $stamp\n##\n" ; set -x
-      oar_resources_add -H $HOSTS_COUNT -C 1 -c 1 -t 1 | tee /tmp/oar_create_resources
+      CPU_COUNT=$(lscpu -J | jq -r '.lscpu | map(select(.field == "Socket(s):")) | .[].data')
+      CORE_COUNT=$(lscpu -J | jq -r '.lscpu | map(select(.field == "Core(s) per socket:")) | .[].data')
+      THREAD_COUNT=$(lscpu -J | jq -r '.lscpu | map(select(.field == "Thread(s) per core:")) | .[].data')
+      oar_resources_add -H $HOSTS_COUNT -C $CPU_COUNT -c $CORE_COUNT -t $THREAD_COUNT | tee /tmp/oar_create_resources
       sync
       . /tmp/oar_create_resources
       touch /tmp/stamp.${stamp// /_}
