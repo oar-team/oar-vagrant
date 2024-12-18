3dae38492e194612ff194983713301a9709af030
Pierre Neyron <pierre.neyron@imag.fr>
2024-12-19 00:07:18 +0100

    g5k wip

--- deb_debian13_pgsql/g5k.sh
+++ deb_debian13_pgsql/g5k.sh
@@ -8,13 +8,10 @@ IP=($(ip -br a | sed -ne 's/^eth.*\s\([[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\
 PREFIX=${IP[0]}
 MASK=${IP[1]}
 
-cat ssh/id_rsa.pub ~/.ssh/authorized_keys > ssh/authorized_keys
 rsync -avz . root@$SERVER:/vagrant
-rsync -rvz ssh/* root@$SERVER:/root/.ssh/
 ssh root@$SERVER 'apt-get update && apt-get -y install iptables jq'
 ssh root@$SERVER /vagrant/provision.sh server $PREFIX $MASK $SERVER $FRONTEND no 1 oar-ftp.imag.fr
 
 rsync -avz . root@$FRONTEND:/vagrant
-rsync -rvz ssh/* root@$FRONTEND:/root/.ssh/
 ssh root@$FRONTEND 'apt-get update && apt-get -y install iptables jq'
 ssh root@$FRONTEND /vagrant/provision.sh frontend $PREFIX $MASK $SERVER $FRONTEND no 1 oar-ftp.imag.fr
