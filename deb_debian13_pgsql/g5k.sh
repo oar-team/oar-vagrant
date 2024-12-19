2bb3d3e4662bee5f7a6d82aa994489acdf3b035d
Pierre Neyron <pierre.neyron@imag.fr>
2024-12-19 10:54:45 +0100

    fixup g5k

--- deb_debian13_pgsql/g5k.sh
+++ deb_debian13_pgsql/g5k.sh
@@ -9,13 +9,12 @@ PREFIX=${IP[0]}
 MASK=${IP[1]}
 
 ssh root@$SERVER "echo -e 'host *\nStrictHostKeyChecking no' > ~/.ssh/config"
-scp -a ~/.ssh/id_rsa* root@$SERVER:.ssh/
+scp ~/.ssh/id_rsa* root@$SERVER:.ssh/
 ssh root@$SERVER 'apt-get update && apt-get -y install iptables jq'
 rsync -avz . root@$SERVER:/vagrant
 ssh root@$SERVER /vagrant/provision.sh server $PREFIX $MASK $SERVER $FRONTEND no 0 oar-ftp.imag.fr
 
 ssh root@$FRONTEND "echo -e 'host *\nStrictHostKeyChecking no' > ~/.ssh/config"
-scp -a ~/.ssh/id_rsa* root@$FRONTEND:.ssh/
-ssh root@$FRONTEND 'apt-get update && apt-get -y install iptables jq'
+scp ~/.ssh/id_rsa* root@$FRONTEND:.ssh/
 rsync -avz . root@$FRONTEND:/vagrant
 ssh root@$FRONTEND /vagrant/provision.sh frontend $PREFIX $MASK $SERVER $FRONTEND no 0 oar-ftp.imag.fr
